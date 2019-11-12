6ec28f7dea57da9c00924539bcaf07be9853311a
Darius Stewart <darius.stewart31@gmail.com>
2019-11-12 06:23:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   array.each do |number|
     squared = number * number
-    new_arrary << squared
+    new_array << squared
     return new_array
-  end 
+  end
 end
